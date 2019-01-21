b2e2c794c921ba22c6cd70464479f9b2f17c82ed
Santiago Guerra <SantiagoGuerra19@gmail.com>
2019-01-21 18:05:13 -0500

    Avatar Component

--- src/stories/index.js
+++ src/stories/index.js
@@ -7,10 +7,16 @@ import {ThemeProvider} from 'styled-components'
 import theme from '../styled.theme'
 
 import Badge from '../components/Badge'
+import Avatar from '../components/Avatar';
 
 storiesOf('UI', module)
   .add('Badge', () => (
     <ThemeProvider theme={theme}>
       <Badge lenght={12}/>
+    </ThemeProvider>
+  ))
+  .add('Avatar', () => (
+    <ThemeProvider theme={theme}>
+      <Avatar url='https://www.rmanj.com/wp-content/uploads/2016/08/avatar-img-03.jpg'/>
     </ThemeProvider>
   ))
