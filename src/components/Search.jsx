ef68d197690358fca002f1e9203074b51e846a5c
Santiago Guerra <SantiagoGuerra19@gmail.com>
2019-01-21 23:58:34 -0500

    Search Component

---
+++ src/components/Search.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const StyledSearchContainer = styled.div`
+  display: flex;
+`
+
+const StyledInputSearch = styled.input.attrs({
+  type: 'search',
+  placeholder: 'Write your Category'
+})`
+  height: 24px;
+  border-radius: 12px 0px 0px 12px;
+  padding-left: ${props => props.theme.normalSpacingSize};
+  background: ${props => props.theme.mainColorTransparent};
+  border: none;
+  font-size: ${props => props.theme.smallFontSize};
+  color: ${props => props.theme.darkColor};
+  font-family: ${props => props.theme.fontFamily};
+
+  &::-webkit-search-cancel-button{
+    -webkit-appearance: none;
+  }
+
+  ::placeholder {
+    color: #FFF;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`
+
+const StyledSearchButton = styled.button`
+  background: ${props => props.theme.mainColor};
+  height: 24px;
+  width: 40px;
+  border: none;
+  border-radius: 0px 12px 12px 0px;
+  color: #FFF;
+
+  &:focus{
+    outline: none;
+  }
+
+  &:hover {
+    opacity: .4;
+    cursor: pointer;
+  }
+`
+
+const Search = ({}) => (
+  <StyledSearchContainer>
+    <StyledInputSearch/>
+    <StyledSearchButton>
+      <FontAwesomeIcon icon='search'/>
+    </StyledSearchButton>
+  </StyledSearchContainer>
+)
+
+export default Search
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 import {ThemeProvider} from 'styled-components'
 import theme from '../styled.theme'
 import Badge from '../components/Badge'
@@ -12,10 +10,10 @@ import AvatarContainer from '../components/AvatarContainer';
 import Header from '../components/Header';
 import '../index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faIgloo, faCoffee, faChevronDown, faChevronUp} from '@fortawesome/free-solid-svg-icons'
+import { faIgloo, faCoffee, faChevronDown, faChevronUp, faSearch} from '@fortawesome/free-solid-svg-icons'
+import Search from '../components/Search';
 
-library.add(faIgloo, faCoffee, faChevronDown, faChevronUp)
+library.add(faIgloo, faCoffee, faChevronDown, faChevronUp, faSearch)
 
 storiesOf('Components', module)
   .add('Badge', () => (
@@ -26,6 +24,11 @@ storiesOf('Components', module)
   .add('Avatar', () => (
     <ThemeProvider theme={theme}>
       <Avatar url='https://www.rmanj.com/wp-content/uploads/2016/08/avatar-img-03.jpg'/>
+    </ThemeProvider>
+  ))
+  .add('Search', () => (
+    <ThemeProvider theme={theme}>
+      <Search/>
     </ThemeProvider>
   ))
   
