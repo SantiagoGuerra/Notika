683e65dc8830d7f7e5b8d2798daa39278da39e80
Santiago Guerra <SantiagoGuerra19@gmail.com>
2019-01-22 21:34:58 -0500

    Configurado el tamaño de fuente y el tamaño del badge para pantallas más grandes

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -17,6 +17,12 @@ const StyledBadge = styled.div`
   z-index: ${props => props.theme.frontLevel};
   font-size: ${props => props.theme.extraSmallFontSize};
   box-shadow: 0px 2px 4px rgba(0, 0, 0, .2);
+
+  @media screen and (min-width: 760px) {
+  font-size: ${props => props.theme.smallFontSize};
+  height: 16px;
+  width: 16px;
+  }
 `
 
 const Badge = ({props, lenght}) => <StyledBadge {...props}>{lenght}</StyledBadge>
