41b866fa3d65fb865898d5a0029d540ab5999995
Santiago Guerra <SantiagoGuerra19@gmail.com>
2019-01-22 20:56:55 -0500

    Add Category Container Component

---
+++ src/components/AddCategoryContainer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import AddCategory from './AddCategory'
+import CategoryItems from './CategoryItems'
+
+const StyledCategoriesContainer = styled.div`
+  padding: ${props => props.theme.normalSpacingSize};
+  border: .5px solid ${props => props.theme.mainColor};
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, .2);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: ${props => props.theme.normalBorder};
+  width: 260px;
+`
+const AddCategoryContainer = ({categories}) => (
+  <StyledCategoriesContainer>
+    <AddCategory/>
+    <CategoryItems categories={categories}/>
+  </StyledCategoriesContainer>
+)
+
+
+export default AddCategoryContainer
