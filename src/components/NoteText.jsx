c9835538f66f8a995ab86c3e9b84e3c8a5a58492
Santiago Guerra <SantiagoGuerra19@gmail.com>
2019-01-22 19:04:28 -0500

    Note Text Component

---
+++ src/components/NoteText.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledNoteText = styled.textarea.attrs({
+  placeholder: 'Your Note'
+})`
+  height: 200px;
+  width: 300px;
+  font-family: ${props => props.theme.fontFamily};
+  color: ${props => props.theme.darkColor};
+  border: .5px solid ${props => props.theme.mainColor};
+  border-radius: 12px;
+  padding-left: ${props => props.theme.normalSpacingSize};
+  font-size: ${props => props.theme.smallFontSize};
+  resize: none;
+  padding: ${props => props.theme.normalSpacingSize};
+
+  &:focus {
+    resize: vertical;
+    outline: none;
+    box-shadow: 0px 2px 4px rgba(0,0,0, .2);
+  }
+
+  ::placeholder {
+    color: ${props => props.theme.lightColor};
+  }
+`
+
+const NoteText = ({}) => (
+  <StyledNoteText>
+    
+  </StyledNoteText>
+)
+
+export default NoteText
+
