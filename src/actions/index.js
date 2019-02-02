01c2dbaa4ec854df0660971dce34174d83e4f368
Santiago Guerra <guerrasantiago19@gmail.com>
2019-02-01 21:33:30 -0500

    Se ha agregado la acción de "SHOW_ADD_CATEGORY"

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const showFormAction = val => ({
   type: 'SHOW_FORM',
   showForm: val
 })
+
+export const showAddCategoryAction = val => ({
+  type: 'SHOW_ADD_CATEGORY',
+  showAddCategory: val
+})
